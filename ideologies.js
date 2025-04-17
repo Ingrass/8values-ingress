a87749b2cec4f7424180d2e130ab3261a18aa312
Ethern Su <ehaprime@gmail.com>
2025-04-17 10:38:21 +0800

    A new start

---
+++ ideologies.js
@@ -0,0 +1,11 @@
+ideologies = [
+    {
+        "name": "测试中",
+        "stats": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    }
+];
