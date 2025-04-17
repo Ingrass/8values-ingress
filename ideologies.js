41dde111092d691369917cdf85f7bd2def196a6b
Ethern Su <ehaprime@gmail.com>
2025-04-17 21:58:11 +0800

    reduced question count; introduced tagging;

--- ideologies.js
+++ ideologies.js
@@ -1,11 +1,146 @@
 ideologies = [
     {
-        "name": "测试中",
+        "name": "极右翼阵营势力",
+        "stats": {
+            "econ": 100,
+            "dipl": 0,
+            "govt": 100,
+            "scty": 0
+        }
+    },
+    {
+        "name": "XM无政府协作者",
+        "stats": {
+            "econ": 0,
+            "dipl": 100,
+            "govt": 0,
+            "scty": 100
+        }
+    },
+    {
+        "name": "阵营保守战术官",
+        "stats": {
+            "econ": 100,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "name": "无道德胡连党",
+        "stats": {
+            "econ": 0,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 0
+        }
+    },
+    {
+        "name": "战术至上主义者",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 100
+        }
+    },
+    {
+        "name": "控制派艺术独狼",
+        "stats": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 100,
+            "scty": 100
+        }
+    },
+    {
+        "name": "作战灵活派铁卫",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "name": "XM权威联盟者",
         "stats": {
             "econ": 0,
             "dipl": 0,
             "govt": 0,
             "scty": 0
         }
-    }
+    },
+    {
+        "name": "秩序中立人士",
+        "stats": {
+            "econ": 100,
+            "dipl": 0,
+            "govt": 100,
+            "scty": 100
+        }
+    },
+    {
+        "name": "XM世界守护人",
+        "stats": {
+            "econ": 0,
+            "dipl": 100,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "name": "极限冲突浪人",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 0
+        }
+    },
+    {
+        "name": "多重信仰特工",
+        "stats": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 100,
+            "scty": 0
+        }
+    },
+    {
+        "name": "控制型协同游牧派",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 0,
+            "scty": 100
+        }
+    },
+    {
+        "name": "铁血秩序阵营党",
+        "stats": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 100
+        }
+    },
+    {
+        "name": "结构主义旅居体",
+        "stats": {
+            "econ": 100,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 100
+        }
+    },
+    {
+        "name": "失序世界散漫派",
+        "stats": {
+            "econ": 0,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 100
+        }
+    },
 ];
--- questions.js
+++ questions.js
@@ -18,10 +18,10 @@ questions = [
         }
     },
     {
-        "question": "link有序不重要，有用才是王道。",
+        "question": "link好看不重要，有用才是王道。",
         "effect": {
             "econ": 10,
-            "dipl": -5,
+            "dipl": 0,
             "govt": 0,
             "scty": 0
         }
@@ -30,16 +30,16 @@ questions = [
         "question": "我曾故意link出一幅“看不懂”的怪图，就为了制造麻烦",
         "effect": {
             "econ": -10,
-            "dipl": 5,
+            "dipl": 0,
             "govt": 0,
             "scty": 0
         }
     },
     {
-        "question": "link对称、面形规整是我link时的基本要求。",
+        "question": "link对称、cf规整是我link时的基本要求。",
         "effect": {
-            "econ": 5,
-            "dipl": -10,
+            "econ": -10,
+            "dipl": 0,
             "govt": 0,
             "scty": 0
         }
@@ -47,8 +47,8 @@ questions = [
     {
         "question": "如果地图全是一堆杂乱link，我会觉得地图被玷污了。",
         "effect": {
-            "econ": 0,
-            "dipl": -10,
+            "econ": -10,
+            "dipl": 0,
             "govt": 0,
             "scty": 0
         }
@@ -81,27 +81,9 @@ questions = [
         }
     },
     {
-        "question": "地图是街头艺术场，我战斗也要风格统一、服装配色讲究。",
-        "effect": {
-            "econ": -10,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
         "question": "我希望Niantic能推出“Intel构图奖”而不是战绩榜。",
         "effect": {
             "econ": -10,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "Portal位置布局的实用性远比規整重要。",
-        "effect": {
-            "econ": 10,
             "dipl": 0,
             "govt": 0,
             "scty": 0
@@ -112,33 +94,6 @@ questions = [
         "effect": {
             "econ": -10,
             "dipl": 0,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "战术最优解胜过一切，哪怕link的很丑”。",
-        "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我会参考历史因素和敌方动向来策划link。",
-        "effect": {
-            "econ": 10,
-            "dipl": -10,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "画出“很复杂”的图形，我会觉得很爽。",
-        "effect": {
-            "econ": -10,
-            "dipl": 5,
             "govt": 0,
             "scty": 0
         }
@@ -162,15 +117,6 @@ questions = [
         }
     },
     {
-        "question": "我反感不对称的link构图，尤其是破坏多层cf图时。",
-        "effect": {
-            "econ": -10,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
         "question": "混沌感也是一种地图美学，它超越了秩序。",
         "effect": {
             "econ": -10,
@@ -180,7 +126,7 @@ questions = [
         }
     },
     {
-        "question": "每次行动前我都会提前查路线、准备MOD和钥匙。",
+        "question": "每次行动前我都会提前查路线及准备道具。",
         "effect": {
             "econ": 0,
             "dipl": -10,
@@ -189,7 +135,7 @@ questions = [
         }
     },
     {
-        "question": "我更喜欢临时起意的冲突戰鬥，不受拘束才带感。",
+        "question": "我更喜欢临时起意的冲突，享受不受拘束的感觉。",
         "effect": {
             "econ": 0,
             "dipl": 10,
@@ -207,47 +153,11 @@ questions = [
         }
     },
     {
-        "question": "最精彩的战斗，往往是“没人带队”时打出来的。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我喜欢把每一次行动复盘、总结、文档化。",
-        "effect": {
-            "econ": 0,
-            "dipl": -10,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "不规划路线，靠感觉打一晚上Ingress很爽。",
+        "question": "不规划路线，靠感觉打一整天Ingress很爽。",
         "effect": {
             "econ": 0,
             "dipl": 10,
             "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我曾因为队友不听调度而感到崩溃或怒退群。",
-        "effect": {
-            "econ": 0,
-            "dipl": -10,
-            "govt": -10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "路线、补给、安排，我更信任自己的判断，而非指挥总控。",
-        "effect": {
-            "econ": 0,
-            "dipl": -5,
-            "govt": 10,
             "scty": 0
         }
     },
@@ -261,29 +171,11 @@ questions = [
         }
     },
     {
-        "question": "我的座右铭是：能打就打，不问后果。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
         "question": "我愿意为了一次行动推掉现实中的聚会或约定。",
         "effect": {
             "econ": 0,
             "dipl": 0,
             "govt": -10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我更享受“打着打着就成形”的战术，而非按部就班的進行。",
-        "effect": {
-            "econ": 0,
-            "dipl": 10,
-            "govt": 0,
             "scty": 0
         }
     },
@@ -297,15 +189,6 @@ questions = [
         }
     },
     {
-        "question": "我有时候不看群、不听指挥，自己一个人打完整场。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
         "question": "我更信赖有组织、有指挥的战斗模式。",
         "effect": {
             "econ": 0,
@@ -315,74 +198,11 @@ questions = [
         }
     },
     {
-        "question": "我的打法随缘，队友跟不跟无所谓，开心就行。",
+        "question": "我习惯XMA的時候独立行动不汇报、不说话，打完走人。",
         "effect": {
             "econ": 0,
             "dipl": 10,
             "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "有明确分工的群体行动，让我更有参与感与成就感。",
-        "effect": {
-            "econ": 0,
-            "dipl": -10,
-            "govt": -10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我参加行动更多是想看热闹，临场才决定出不出手。",
-        "effect": {
-            "econ": 0,
-            "dipl": 10,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我对“战术执行力”这件事有较高执念。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": -10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我习惯XMA的時候开车不汇报、不说话，打完走人。",
-        "effect": {
-            "econ": 0,
-            "dipl": 10,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "一个人拆一大片敌方大农场（物资8级po），是我最爽的时刻。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我更喜欢在任务中扮演“后勤补给”“钥匙传送”这种团队支援角色。",
-        "effect": {
-            "econ": 0,
-            "dipl": -5,
-            "govt": -10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我曾因队友挂机划水而怒退行动群。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": -10,
             "scty": 0
         }
     },
@@ -391,24 +211,6 @@ questions = [
         "effect": {
             "econ": 0,
             "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我做事习惯记录计划、准备文档、传战术笔记给团队。",
-        "effect": {
-            "econ": 0,
-            "dipl": -10,
-            "govt": -5,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我宁可单干一晚上，也不想被战术分工限制。",
-        "effect": {
-            "econ": 0,
-            "dipl": 5,
             "govt": 10,
             "scty": 0
         }
@@ -427,7 +229,7 @@ questions = [
         "effect": {
             "econ": 0,
             "dipl": 10,
-            "govt": 10,
+            "govt": 5,
             "scty": 0
         }
     },
@@ -459,29 +261,11 @@ questions = [
         }
     },
     {
-        "question": "我觉得自己一个人也能左右战局。",
-        "effect": {
-            "econ": 0,
-            "dipl": 5,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
         "question": "我乐于担任行动指挥或频道调度这种协调类角色。",
         "effect": {
             "econ": 0,
             "dipl": -5,
             "govt": -10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "哪怕队友都躺了，我一个人也能坚持打完。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
             "scty": 0
         }
     },
@@ -495,33 +279,6 @@ questions = [
         }
     },
     {
-        "question": "我打战时很少说话，默默完成目标是我的风格。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我觉得没有协作就没有胜利。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": -15,
-            "scty": 0
-        }
-    },
-    {
-        "question": "团队任务中，我喜欢自己选干的部分，而不是被安排。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 5,
-            "scty": 0
-        }
-    },
-    {
         "question": "link之前，我会实现咨询大家意见，不喜欢独断。",
         "effect": {
             "econ": 0,
@@ -531,30 +288,12 @@ questions = [
         }
     },
     {
-        "question": "我的玩法核心是“独立自主，自由输出”。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "哪怕对面是朋友，在战场上我也不会手软。",
+        "question": "哪怕对方阵营的对手是朋友，在战场上我也不会手软。",
         "effect": {
             "econ": 0,
             "dipl": 0,
             "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我曾私下放过对方阵营的portal，因为现实中portal的所有者是我朋友。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 5
+            "scty": -10
         }
     },
     {
@@ -573,24 +312,6 @@ questions = [
             "dipl": 0,
             "govt": 0,
             "scty": 10
-        }
-    },
-    {
-        "question": "我曾因为队友背刺过我而想退坑或转阵营。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 10
-        }
-    },
-    {
-        "question": "我愿意为阵营胜利，牺牲现实生活的规划。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 0,
-            "scty": -10
         }
     },
     {
@@ -639,7 +360,7 @@ questions = [
         }
     },
     {
-        "question": "我只愿意和本阵营友軍活动，不会和敵方蹭活动点。",
+        "question": "我只愿意和本阵营友军活动，不会和对方玩家互刷。",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -657,15 +378,6 @@ questions = [
         }
     },
     {
-        "question": "我曾因阵营内部矛盾而独自脱离社群。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 10
-        }
-    },
-    {
         "question": "我加入Ingress一开始就觉得“阵营像宗教”。",
         "effect": {
             "econ": 0,
@@ -676,15 +388,6 @@ questions = [
     },
     {
         "question": "我希望能有中立第三阵营或跳出二元对抗的机制。",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 10
-        }
-    },
-    {
-        "question": "我曾被人质疑忠诚度，这让我一度疏远了阵营活动。",
         "effect": {
             "econ": 0,
             "dipl": 0,
@@ -720,93 +423,21 @@ questions = [
         }
     },
     {
-        "question": "我会在规划战术时追求既高效又好看，即使牺牲一点也要保留整洁美观。",
-        "effect": {
-            "econ": -10,
-            "dipl": 5,
-            "govt": 0,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我觉得阵营只是载体，我更关注能否自由表达自己喜欢的游戏方式。",
-        "effect": {
-            "econ": -5,
-            "dipl": 5,
-            "govt": 0,
-            "scty": 5
-        }
-    },
-    {
-        "question": "比起和大家一起打游戏或指挥，我更想一个人静静研究敌方的策略和游戏频率。",
-        "effect": {
-            "econ": 10,
-            "dipl": 0,
-            "govt": 5,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我参加行动不是为了听命令，而是因为我能“自己决定我打哪”。",
-        "effect": {
-            "econ": 0,
-            "dipl": 10,
-            "govt": 5,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我愿意投入大量时间研究行动战术，只要这能为阵营赢下决胜点。",
-        "effect": {
-            "econ": 0,
-            "dipl": -10,
-            "govt": 0,
-            "scty": -10
-        }
-    },
-    {
         "question": "我愿意坚持参加阵营行动，哪怕战术混乱、指挥拉胯，我也要撑到最后。",
         "effect": {
             "econ": 0,
-            "dipl": -10,
-            "govt": 0,
+            "dipl": -5,
+            "govt": -5,
             "scty": -10
-        }
-    },
-    {
-        "question": "我一个人拆了三片大田，没人点赞也无所谓，我更在乎地图干净。",
-        "effect": {
-            "econ": -10,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
-        }
-    },
-    {
-        "question": "我崇尚随便link和所属阵营精神，但也希望对抗比赛的时候不要输。",
-        "effect": {
-            "econ": -10,
-            "dipl": 0,
-            "govt": 0,
-            "scty": -10
-        }
-    },
-    {
-        "question": "我属于那种“XJBL（胡连党）+不听指挥”的玩家，但也不妨碍我完成多重cf。",
-        "effect": {
-            "econ": -10,
-            "dipl": 0,
-            "govt": 10,
-            "scty": 0
         }
     },
     {
         "question": "我玩Ingress是为了探索、表达、参与社区，而不是非要赢。",
         "effect": {
             "econ": -10,
-            "dipl": 10,
-            "govt": 10,
-            "scty": 10
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
         }
     }
 ]
